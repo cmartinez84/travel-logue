ca929cf217f8421c72aaed928db88966fcfcdcc5
chris martinez and katrina hirsch <katrina@>
2016-08-22 14:36:52 -0700

    will accept all fields of input and print trip info when trip ID is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 //Business Logic
-function Place(destination) {
+function Place(destination, date, landmarks, eats, notes) {
   this.destination = destination;
-  this.landmarks = landmarks;
   this.date = date;
+  this.landmarks = landmarks;
+  this.eats = eats;
   this.notes = notes;
 }
 
@@ -10,4 +11,28 @@ Place.prototype.tripID = function(){
   return this.destination + " " + this.date;
 }
 
+var placesArray =[];
 //User Interface Logic
+$(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var destination = $("#destination").val();
+    var date = $("#date").val();
+    var landmarks = $("#landmarks").val();
+    var eats = $("#eats").val();
+    var notes = $("#notes").val();
+    var newPlace = new Place(destination, date, landmarks, eats, notes);
+    placesArray.push(newPlace);
+    $("#list").append("<li>"+newPlace.tripID()+"</li>");
+    
+
+    $("li").click(function() {
+
+      $("#log").show();
+      $("#log h2").text(newPlace.tripID());
+      $(".landmarks").text(newPlace.landmarks);
+      $(".eats").text(newPlace.eats);
+      $(".notes").text(newPlace.notes);
+    });
+  });
+});
